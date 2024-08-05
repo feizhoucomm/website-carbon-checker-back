e99d6593415d402e4b2c10d02530d6779defc9a4
ousskhayi <khayi.ouss@gmail.com>
2024-08-05 15:58:46 +0100

    update -cors- index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: "*", // Allow requests from any origin
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET"],
     allowedHeaders: ["Content-Type"],
   })
 );
